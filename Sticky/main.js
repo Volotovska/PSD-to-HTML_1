d3764b35aeeb203f25c5d4f8b409a05c0fe256c6
Anna Volotovska <ann.volotovska@gmail.com>
2018-09-12 21:32:15 +0300

    Update main.js

--- Sticky/main.js
+++ Sticky/main.js
@@ -1,10 +1,7 @@
-
-var dif = 10;
-
 $('#progressBar').click(function(){
     var initial_width = $("#progressBar").css('width');
-    var added = 10px;
-    var new_width = initial_width + added;
-    $("#progressBar").css('width', 'new_width');
+    var added = 10;
+    var new_width = parseInt(initial_width) + added;
+    $("#progressBar").css('width', new_width);
     console.log(new_width);
 });
