366f3e75a180c4aa03da14470ab0ec2ebcbfeedc
HydraOrc <ann.volotovska@gmail.com>
2018-10-26 19:51:11 +0300

    updated files

--- Online Shop/main.js
+++ Online Shop/main.js
@@ -52,3 +52,9 @@ logo2.addEventListener('click', showMenuFunc);
 function showMenuFunc() {
    menuContainer.classList.add('shown');
 }
+
+menuContainer.addEventListener('click', closeMenuFunc);
+
+function closeMenuFunc() {
+   menuContainer.classList.remove('shown');
+}
